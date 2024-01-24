5123d0fa4e6ba556988a26f3b25c009972ed5497
MaleneRJohansen <malene9@hotmail.no>
2024-01-24 15:55:38 +0100

    Nye HTML sider
    Oppdatert HTML sider og kode. Oppdatert js layout.

--- layout.js
+++ layout.js
@@ -2,14 +2,12 @@ console.log(resources)
 
 let resourcesHTML = ""
 
-resources.map(resource => resourcesHTML += `<article>
-<h2><a href="#">${resource.category}</a></h2>
-<h3>${resource.category}</h3>
-<p>${resource.text}</p>
-<ul><li><a href="${resource.sources["url"]}">${resource.sources["title"]}</a></li></ul>
-</article>`)
+const page = ["index.html", "css.html", "javascript.html", "react.html", "sandhcms.html"]
+
+resources.map(resource => resourcesHTML += `<article><h2><a href="#">${resource.category}</a></h2></article>`)
+//<h2><a href="#">${resource.category}</a></h2><h3>${resource.category}</h3><p>${resource.text}</p><ul><li><a href="${resource.sources["url"]}">${resource.sources["title"]}</a></li></ul>
 
 console.log(resourcesHTML)
 
-const body = document.getElementsByTagName("body")
-body[0].innerHTML = resourcesHTML
+const nav = document.getElementsByTagName("nav")
+nav[0].innerHTML = resourcesHTML
