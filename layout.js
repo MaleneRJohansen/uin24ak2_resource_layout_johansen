ca5bcd85039bc939a9640fd5dd0456a60e45088b
MaleneRJohansen <malene9@hotmail.no>
2024-01-25 15:15:56 +0100

    CSS oppdatert og layout.js oppdatert
    javascript skal delvis funke. Ikke helt riktig, men nesten der

--- layout.js
+++ layout.js
@@ -2,8 +2,6 @@ console.log(resources)
 
 let resourcesHTML = ""
 
-const page = ["index.html", "css.html", "javascript.html", "react.html", "sandhcms.html"]
-
 resources.map(resource => resourcesHTML += `<article><h2><a href="#">${resource.category}</a></h2></article>`)
 //<h2><a href="#">${resource.category}</a></h2><h3>${resource.category}</h3><p>${resource.text}</p><ul><li><a href="${resource.sources["url"]}">${resource.sources["title"]}</a></li></ul>
 
@@ -11,3 +9,9 @@ console.log(resourcesHTML)
 
 const nav = document.getElementsByTagName("nav")
 nav[0].innerHTML = resourcesHTML
+
+const body = document.getElementsByTagName("body")
+
+if (resources.category === "HTML") {
+
+}
