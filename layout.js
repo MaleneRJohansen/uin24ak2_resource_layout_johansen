efd679d6d063210f5bca556cc7de8e947ed4f898
MaleneRJohansen <malene9@hotmail.no>
2024-01-26 10:26:59 +0100

    clean start med html og js
    Endret HTML-kode og begynt på nytt med js.

--- layout.js
+++ layout.js
@@ -1,17 +1,1 @@
 console.log(resources)
-
-let resourcesHTML = ""
-
-resources.map(resource => resourcesHTML += `<article><h2><a href="#">${resource.category}</a></h2></article>`)
-//<h2><a href="#">${resource.category}</a></h2><h3>${resource.category}</h3><p>${resource.text}</p><ul><li><a href="${resource.sources["url"]}">${resource.sources["title"]}</a></li></ul>
-
-console.log(resourcesHTML)
-
-const nav = document.getElementsByTagName("nav")
-nav[0].innerHTML = resourcesHTML
-
-const body = document.getElementsByTagName("body")
-
-if (resources.category === "HTML") {
-
-}
