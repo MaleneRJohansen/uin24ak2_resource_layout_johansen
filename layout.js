938929899fb8a19e0571f58ea700feeb7c10dcc0
MaleneRJohansen <malene9@hotmail.no>
2024-01-26 11:04:45 +0100

    Oppdeling av js funker!
    Første del av js koden funker

--- layout.js
+++ layout.js
@@ -1,1 +1,27 @@
-console.log(resources)
+let printOut = ""
+const documentName = window.location.pathname.split("/").pop()
+//Linje 2 er kode jeg har fått fra ChatGPT da jeg spurte hvordan sjekke navnet på dokumentet. "How to check the name of the document"
+
+if (documentName ==="index.html") {
+    const HTMLresource = resources[0]
+
+    printOut += `<h2>${HTMLresource.category}</h2><p>${HTMLresource.text}</p><ul>`
+
+    HTMLresource.sources.map((source) => {
+        printOut += `<li><a href="${source.url}">${source.title}</a></li>`
+    })
+
+    printOut += `</ul>`
+} else {
+    resources.map((resource) => {
+        printOut += `<h2>${resource.category}</h2><p>${resource.text}</p><ul>`
+    })
+
+    resource.sources.map((source) => {
+        printOut += `<li><a href="${source.url}">${source.title}</a></li>`
+    })
+
+    printOut += `</ul>`
+}
+
+document.getElementById("content").innerHTML = printOut
